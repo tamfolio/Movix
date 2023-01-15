81ca65b8547fb2bd9461ea2d3eb88db936c0b441
Tamilore <tamiloreolaniyi@gmail.com>
2023-01-15 22:55:41 +0100

    movie component

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Homepage.css'
 import Nav from '../Nav/Nav'
 import Banner from '../Banner/Banner'
+import Movies from '../Movies/Movies'
 
 function Homepage() {
   return (
     <div className='homepage'>
       <Nav/>
       <Banner/>
+      <Movies heading='Featured Movies' img="/assets/movie.png" releaseDate='USA, 2016 - Current' title='Stranger Things'/>
+      <Movies heading='New Arrival' img="/assets/movie2.png" releaseDate='USA, 2021' title='Dune'/>
     </div>
   )
 }
--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -1,9 +1,123 @@
 import React from 'react'
+import './movies.css'
 
-function Movies() {
+function Movies({heading, img,releaseDate, title}) {
   return (
-    <div>
-      
+    <div className='Movie-container'>
+      <div className="top">
+        <h1>{heading}</h1>
+        <p>See all &gt;</p>
+      </div>
+      <div className="Movie-details">
+        <div className="movie-box">
+            <img src={img} alt="" />
+            <div className="movie-desc">
+                <h3 className="release-date">{releaseDate}</h3>
+                <h2 className="movie-title">{title}</h2>
+                <div className="movie-ratings">
+                <div className="imdb">
+                    <img src="/assets/imdb.svg" alt="" />
+                    <p>86.0 / 100</p>
+                </div>
+                <div className="imdb">
+                    <img src="/assets/apple.svg" alt="" />
+                    <p>97%</p>
+                </div>
+                </div>
+                <h4 className="movie-category">Action, Adventure, Horror</h4>
+            </div>
+        </div>
+        <div className="movie-box">
+            <img src={img} alt="" />
+            <div className="movie-desc">
+                <h3 className="release-date">{releaseDate}</h3>
+                <h2 className="movie-title">{title}</h2>
+                <div className="movie-ratings">
+                <div className="imdb">
+                    <img src="/assets/imdb.svg" alt="" />
+                    <p>86.0 / 100</p>
+                </div>
+                <div className="imdb">
+                    <img src="/assets/apple.svg" alt="" />
+                    <p>97%</p>
+                </div>
+                </div>
+                <h4 className="movie-category">Action, Adventure, Horror</h4>
+            </div>
+        </div>
+        <div className="movie-box">
+            <img src={img} alt="" />
+            <div className="movie-desc">
+                <h3 className="release-date">{releaseDate}</h3>
+                <h2 className="movie-title">{title}</h2>
+                <div className="movie-ratings">
+                <div className="imdb">
+                    <img src="/assets/imdb.svg" alt="" />
+                    <p>86.0 / 100</p>
+                </div>
+                <div className="imdb">
+                    <img src="/assets/apple.svg" alt="" />
+                    <p>97%</p>
+                </div>
+                </div>
+                <h4 className="movie-category">Action, Adventure, Horror</h4>
+            </div>
+        </div>
+        <div className="movie-box">
+            <img src={img} alt="" />
+            <div className="movie-desc">
+                <h3 className="release-date">{releaseDate}</h3>
+                <h2 className="movie-title">{title}</h2>
+                <div className="movie-ratings">
+                <div className="imdb">
+                    <img src="/assets/imdb.svg" alt="" />
+                    <p>86.0 / 100</p>
+                </div>
+                <div className="imdb">
+                    <img src="/assets/apple.svg" alt="" />
+                    <p>97%</p>
+                </div>
+                </div>
+                <h4 className="movie-category">Action, Adventure, Horror</h4>
+            </div>
+        </div>
+        <div className="movie-box">
+            <img src={img} alt="" />
+            <div className="movie-desc">
+                <h3 className="release-date">{releaseDate}</h3>
+                <h2 className="movie-title">{title}</h2>
+                <div className="movie-ratings">
+                <div className="imdb">
+                    <img src="/assets/imdb.svg" alt="" />
+                    <p>86.0 / 100</p>
+                </div>
+                <div className="imdb">
+                    <img src="/assets/apple.svg" alt="" />
+                    <p>97%</p>
+                </div>
+                </div>
+                <h4 className="movie-category">Action, Adventure, Horror</h4>
+            </div>
+        </div>
+        <div className="movie-box">
+            <img src={img} alt="" />
+            <div className="movie-desc">
+                <h3 className="release-date">{releaseDate}</h3>
+                <h2 className="movie-title">{title}</h2>
+                <div className="movie-ratings">
+                <div className="imdb">
+                    <img src="/assets/imdb.svg" alt="" />
+                    <p>86.0 / 100</p>
+                </div>
+                <div className="imdb">
+                    <img src="/assets/apple.svg" alt="" />
+                    <p>97%</p>
+                </div>
+                </div>
+                <h4 className="movie-category">Action, Adventure, Horror</h4>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }

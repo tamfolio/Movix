3bd07d25e49e24cd3a5c3f08934b0a89835d33fa
Tamilore <tamiloreolaniyi@gmail.com>
2023-01-15 14:31:10 +0100

    register form

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import './Register.css'
+import React from "react";
+import "./Register.css";
 
 function Register() {
   return (
-    <div className='registerpage'>
+    <div className="registerpage">
       <div className="register-container">
         <div className="movix-logo">
-            <img src="/assets/movix_logo.png" alt="" />
+          <img src="/assets/movix_logo.png" alt="" />
         </div>
-        <h1>Hi, Welcome</h1>
-        <p>Please sign-in to your account and start your experience</p>
+        <h1 className="register-heading">Hi, Welcome</h1>
+        <p className="register-text">
+          Please sign-in to your account and start your experience
+        </p>
+        <div className="register-form">
+          <input type="text" placeholder="Full Name" />
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>REGISTER</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;

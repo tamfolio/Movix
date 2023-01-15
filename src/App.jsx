a6fa44fa350bdf9eda60d418c34858d135e8e1de
Tamilore <tamiloreolaniyi@gmail.com>
2023-01-15 14:14:15 +0100

    fonts and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Register from './Components/Register/Register'
 
 function App() {
 
   return (
     <div className="App">
-        
+        <Register/>
     </div>
   )
 }
---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Register.css'
+
+function Register() {
+  return (
+    <div className='registerpage'>
+      <div className="register-container">
+        <div className="movix-logo">
+            <img src="/assets/movix_logo.png" alt="" />
+        </div>
+        <h1>Hi, Welcome</h1>
+        <p>Please sign-in to your account and start your experience</p>
+      </div>
+    </div>
+  )
+}
+
+export default Register
